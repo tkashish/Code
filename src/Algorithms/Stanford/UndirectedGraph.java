175870ad2626f74d86d28d85974f9fec5c2b3e4e
tkashish <ktkashish@gmail.com>
2017-01-24 20:48:06 -0800

    Dijkstra implementation for graphs with positive edges

--- src/Algorithms/Stanford/UndirectedGraph.java
+++ src/Algorithms/Stanford/UndirectedGraph.java
@@ -7,14 +7,14 @@ import java.util.*;
 /**
  * Created by kashishtayal on 12/20/16.
  */
-public class MinimiumSpanningTree {
+public class UndirectedGraph {
     Map<Integer, List<Edge>> adjList = new HashMap<>();
     int numOfVertices = 0;
-    public MinimiumSpanningTree(String inFileName){
+    public UndirectedGraph(String inFileName){
         init(inFileName);
     }
     private void init(String inFileName){
-        URL path = MinimiumSpanningTree.class.getClassLoader().getResource(inFileName);
+        URL path = UndirectedGraph.class.getClassLoader().getResource(inFileName);
         try {
             List<String> lines = FIleReaderUtils.fileReader(path);
             for(int i = 1; i < lines.size(); i++){
@@ -73,6 +73,52 @@ public class MinimiumSpanningTree {
         }
         return result;
     }
+    public int shortestPathLengthBetween(int inV1, int inV2){
+        Map<Integer,Integer> followMap = new HashMap<>();
+        PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] < o2[1]) return -1;
+                else if(o1[1] > o2[1]) return 1;
+                else return 0;
+            }
+        });
+
+        Set<Integer> visited = new HashSet<>();
+        heap.add(new int[]{inV1,0,inV1});
+        int result = -1;
+        while(visited.size() < adjList.size()){
+            int[] vert = heap.poll();
+            int id = vert[0];
+            int dist = vert[1];
+            int prev = vert[2];
+            while(visited.contains(id)){
+                vert = heap.poll();
+                id = vert[0];
+                dist = vert[1];
+                prev = vert[2];
+            }
+            followMap.put(id,prev);
+            visited.add(id);
+            if(id == inV2){
+                result = dist;
+            }
+            List<Edge> neighbors = adjList.get(id);
+            for(Edge edge : neighbors){
+                int neighborId = edge.toVertex(id);
+                if(!visited.contains(neighborId)){
+                    heap.add(new int[]{neighborId,dist+edge.weight,id});
+                }
+            }
+        }
+        int curr = inV2;
+        while(curr != inV1){
+            System.out.print(curr+" <- ");
+            curr = followMap.get(curr);
+        }
+        System.out.println(inV1);
+        return result;
+    }
     class Edge implements Comparable<Edge>{
         int vertex1;
         int vertex2;
@@ -92,8 +138,9 @@ public class MinimiumSpanningTree {
             else return 0;
         }
     }
+
     public static void main(String[] args) {
-        MinimiumSpanningTree mst = new MinimiumSpanningTree("edges.txt");
-        System.out.println(mst.calculateMSTCost());
+        UndirectedGraph graph = new UndirectedGraph("dijkstraTest.txt");
+        System.out.println(graph.shortestPathLengthBetween(1,5));
     }
 }
