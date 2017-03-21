9232aee02992e6807eee123840d915232ed3edcb
tkashish <ktkashish@gmail.com>
2017-03-20 18:57:24 -0700

    BestTimeTOBuyAndSellStockWithCoolDown

--- src/LeetCode/DynamicPrograming/BestTimeToBuySellStocks.java
+++ src/LeetCode/DynamicPrograming/BestTimeToBuySellStocks.java
@@ -1,7 +1,5 @@
 package LeetCode.DynamicPrograming;
 
-import java.util.Arrays;
-
 /**
  * Created by kashishtayal on 11/13/16.
  */
@@ -39,6 +37,29 @@ public class BestTimeToBuySellStocks {
     public static void main(String[] args) {
         BestTimeToBuySellStocks best = new BestTimeToBuySellStocks();
         System.out.println(best.maxProfit(new int[]{1,4,2}));
-        System.out.println(best.maxProfit(new int[]{1,2,3,0,2}));
+        System.out.println(buy(new int[]{1,2,3,0,2}));
+    }
+
+    private static int max(int... vals){
+        int max = Integer.MIN_VALUE;
+        for(int i : vals){
+            max = Math.max(max,i);
+        }
+        return max;
+    }
+
+    private static int buy(int[] value){
+        int buyMax = Integer.MIN_VALUE;
+        int lastMaxCoolDown = 0;
+        int maxSum = 0;
+        for(int i = 0; i < value.length; i++){
+            int buyToday = lastMaxCoolDown-value[i];
+            int sellToday = buyMax+value[i];
+            int coolDown = maxSum;
+            maxSum = max(maxSum,buyToday,sellToday,coolDown);
+            buyMax = max(buyMax,buyToday);
+            lastMaxCoolDown = max(lastMaxCoolDown,coolDown);
+        }
+        return maxSum;
     }
 }
